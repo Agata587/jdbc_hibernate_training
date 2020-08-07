a28002582a40acae8e5652591080f00d43ac3928
Agata <agata.wloszek@gmail.com>
2020-08-07 20:08:28 +0200

    Parametrized Query Demo

--- src/main/java/pl/sda/EstabilishConnection2.java
+++ src/main/java/pl/sda/EstabilishConnection2.java
@@ -1,8 +1,10 @@
 package pl.sda;
 
+import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.Properties;
 import java.util.TimeZone;
 
@@ -27,10 +29,9 @@ public class EstabilishConnection2 {
 
 
 
+        //Zadanie 1 -> przykład z pierwszym pobraniem danych
 
-
-
-       final String query = "select customerName, country from customers"; //musimy zdefiniować Query;
+       /*final String query = "select customerName, country from customers"; //musimy zdefiniować Query;
         //można ten select przekopiować z MySQL
 
         try(Connection connection = DriverManager.getConnection(   //tutaj mamy połączenie z bazą które wcześniej utworzyliśmy
@@ -57,8 +58,70 @@ public class EstabilishConnection2 {
             System.err.println("Blad nawiazywania polaczenia z baza danych: " + sex);
         }
 
+    }*/
+
+
+
+
+        //Zadanie 2 -> chcemy wyświetlić payments za dany rok, powyżej jakiejś kwoty.
+
+        try(Connection connection = DriverManager.getConnection(   //tutaj mamy połączenie z bazą które wcześniej utworzyliśmy
+                configuration.get("db.url").toString() + "?serverTimezone=" + TimeZone.getDefault().getID(),
+                configuration.get("db.user").toString(),
+                configuration.get("db.pswd").toString());
+
+
+        ) {
+            System.out.println("Nawiazano polaczenie z baza danych.");
+
+            printPaymentsForYearAndAmountAbove(2004, 10000.0, connection);
+
+        }
+        catch(SQLException sex) {
+            System.err.println("Blad nawiazywania polaczenia z baza danych: " + sex);
+        }
 
     }
+
+
+
+
+    public static void printPaymentsForYearAndAmountAbove (final int year, final double minAmount, final Connection connection) {
+
+        String parametrizedQuery = "select * from payments where paymentDate between ? and ? and amount > ?";
+                                            // wklejamy komende z SQL'a i zamiast kontretnych parametrow dajemy "?"
+
+
+       try(PreparedStatement preparedStatement = connection.prepareStatement(parametrizedQuery)){
+           preparedStatement.setDate(1, Date.valueOf(LocalDate.of(year,1,1)));  //pierwszy znak zapytania
+           preparedStatement.setDate(2, Date.valueOf(LocalDate.of(year,12,31)));  //drugi znak zapytania
+           preparedStatement.setDouble(3, minAmount);  // trzeci znak zapytania
+
+
+           ResultSet resultSet = preparedStatement.executeQuery();
+
+           System.out.printf("Oto palatnosci za rok %s w kwocie przekraczającej %f\n", year, minAmount); //PrintStream w formacie
+
+
+           while(resultSet.next()) {
+               System.out.println("Customer number | Check number | Payment Date | Amount");
+               System.out.printf("%s | %s | %s | %f\n",               // printf to jest w formacie!!!!
+                       resultSet.getString(1),
+                       resultSet.getString(2),
+                       resultSet.getString(3),
+                       resultSet.getDouble(4));
+           }
+
+       }
+       catch(SQLException sex) {
+           System.err.println("Blad odczytu z bazy danych:" + sex);
+       }
+
+
+    }
+
+
+
 
 
 
