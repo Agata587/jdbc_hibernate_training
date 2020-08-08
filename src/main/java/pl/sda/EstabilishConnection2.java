6959000232179dea8b8b649d1c1344418ff42824
Agata <agata.wloszek@gmail.com>
2020-08-08 22:16:50 +0200

    Demo - converting from ResultSet to domain entities

--- src/main/java/pl/sda/EstabilishConnection2.java
+++ src/main/java/pl/sda/EstabilishConnection2.java
@@ -1,10 +1,15 @@
 package pl.sda;
 
+import pl.sda.domain.Payment;
+import pl.sda.domain.ProductLine;
+
 import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 import java.util.TimeZone;
 
@@ -31,7 +36,7 @@ public class EstabilishConnection2 {
 
         //Zadanie 1 -> przykład z pierwszym pobraniem danych
 
-       /*final String query = "select customerName, country from customers"; //musimy zdefiniować Query;
+       final String query = "select customerName, country from customers"; //musimy zdefiniować Query;
         //można ten select przekopiować z MySQL
 
         try(Connection connection = DriverManager.getConnection(   //tutaj mamy połączenie z bazą które wcześniej utworzyliśmy
@@ -39,43 +44,23 @@ public class EstabilishConnection2 {
                 configuration.get("db.user").toString(),
                 configuration.get("db.pswd").toString());
 
-            Statement statement = connection.createStatement(); //Statement czyli to zapytanie w kontekście Connection
+            /*Statement statement = connection.createStatement(); //Statement czyli to zapytanie w kontekście Connection
             ResultSet result = statement.executeQuery(query);  //ResultSet czyli wynik który nam zwróci executeQuery
                                                                 // ładujemy Query które zdefiniowaliśmy wyżej w final String
-
+*/
             //Te dane trzeba teraz jakoś obrobić w SOUT
         ) {
             System.out.println("Nawiazano polaczenie z baza danych.");
-            System.out.println("Oto nasi klienci z podziałem na kraje");
-            System.out.println("Client name | Country");
+            /*System.out.println("Oto nasi klienci z podziałem na kraje");
+            System.out.println("Client name | Country");*/
 
-
+/*
             while(result.next()) {
                 System.out.println(result.getString("customerName") + " | " + result.getString("country"));
-            }
-        }
-        catch(SQLException sex) {
-            System.err.println("Blad nawiazywania polaczenia z baza danych: " + sex);
-        }
+            }*/
 
-    }*/
-
-
-
-
-        //Zadanie 2 -> chcemy wyświetlić payments za dany rok, powyżej jakiejś kwoty.
-
-        try(Connection connection = DriverManager.getConnection(   //tutaj mamy połączenie z bazą które wcześniej utworzyliśmy
-                configuration.get("db.url").toString() + "?serverTimezone=" + TimeZone.getDefault().getID(),
-                configuration.get("db.user").toString(),
-                configuration.get("db.pswd").toString());
-
-
-        ) {
-            System.out.println("Nawiazano polaczenie z baza danych.");
-
-            printPaymentsForYearAndAmountAbove(2004, 10000.0, connection);
-
+            List<Payment> payments = retrievePaymentsForYearAndAmountAbove(2004, 10000.0, connection);
+            System.out.println("10ta pozycja na liście to: " + payments.get(9));
         }
         catch(SQLException sex) {
             System.err.println("Blad nawiazywania polaczenia z baza danych: " + sex);
@@ -84,6 +69,9 @@ public class EstabilishConnection2 {
     }
 
 
+
+
+        //Zadanie 2 -> chcemy wyświetlić payments za dany rok, powyżej jakiejś kwoty.
 
 
     public static void printPaymentsForYearAndAmountAbove (final int year, final double minAmount, final Connection connection) {
@@ -108,7 +96,7 @@ public class EstabilishConnection2 {
                System.out.printf("%s | %s | %s | %f\n",               // printf to jest w formacie!!!!
                        resultSet.getString(1),
                        resultSet.getString(2),
-                       resultSet.getString(3),
+                       resultSet.getDate(3),
                        resultSet.getDouble(4));
            }
 
@@ -119,6 +107,45 @@ public class EstabilishConnection2 {
 
 
     }
+
+
+        public static List<Payment> retrievePaymentsForYearAndAmountAbove(final int year, final double minAmount, final Connection connection){
+
+            String parametrizedQuery = "select * from payments WHERE paymentDate BETWEEN ? AND ? AND amount > ?";
+            // wklejamy komende z SQL'a i zamiast kontretnych parametrow dajemy "?"
+
+            List<Payment> payments = new ArrayList<>();
+
+            try ( PreparedStatement preparedStatement = connection.prepareStatement(parametrizedQuery)){
+                preparedStatement.setDate(1, Date.valueOf(LocalDate.of(year,1,1)));  //pierwszy znak zapytania
+                preparedStatement.setDate(2, Date.valueOf(LocalDate.of(year,12,31)));  //drugi znak zapytania
+                preparedStatement.setDouble(3, minAmount);  // trzeci znak zapytania
+
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+
+                while(resultSet.next()) {
+                   payments.add(new Payment( resultSet.getString(1),
+                                            resultSet.getString(2),
+                                            resultSet.getDate(3),
+                                            resultSet.getDouble(4)));
+                }
+            }
+            catch(SQLException sex) {
+                System.err.println("Blad odczytu z bazy danych:" + sex);
+            }
+            return payments;
+        }
+
+
+
+
+     
+
+
+
+
+
 
 
 
--- src/main/java/pl/sda/domain/Payment.java
+++ src/main/java/pl/sda/domain/Payment.java
@@ -1,8 +1,47 @@
 package pl.sda.domain;
 
+import java.time.LocalDate;
+
 public class Payment {
 
 
-    private String customerName, checkNo;
+    private final String customerName, checkNo;
+    private final LocalDate date;
+    private final double amount;
+
+    public Payment (String customerName, String checkNo, java.sql.Date date, double amount) {
+        this.customerName = customerName;
+        this.checkNo = checkNo;
+        this.date = date.toLocalDate();
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "customerName='" + customerName + '\'' +
+                ", checkNo='" + checkNo + '\'' +
+                ", date=" + date +
+                ", amount=" + amount +
+                '}';
+    }
+
+    public String getCustomerName () {
+        return customerName;
+    }
+
+    public String getCheckNo () {
+        return checkNo;
+    }
+
+    public LocalDate getDate (){
+        return date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+
 
 }
